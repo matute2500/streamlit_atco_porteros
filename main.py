03ea1ebcc4b64fabee3e59d5c4577f997cea51a7
matute2500 <36451218+matute2500@users.noreply.github.com>
2024-10-16 13:24:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,88 +1,116 @@
 import streamlit as st
 import pandas as pd
+import supabase
 from supabase import create_client, Client
-import gdown
+from json import loads, dumps
 
-id = "14LD9iisP2aqx9tmypqKq7lGMmC853Rwn"
-output = "archivo.csv"
-gdown.download(id=id, output=output)
-#df_clubs = pd.read_csv(output)
-df_clubs = pd.read_csv(output, index_col=0, sep=';', encoding='latin-1')
-st.write(df_clubs)
+# Inyectar CSS personalizado
+css = """
+<style>
+    .stDataFrame th {
+        background-color: #4CAF50;
+        color: white;
+        font-family: Arial, sans-serif;
+        font-size: 16px;
+    }
+    .stDataFrame td, .stDataFrame th {
+        border: 2px solid #4CAF50;
+    }
+</style>
+"""
 
-
+st.markdown(css, unsafe_allow_html=True)
 
 url: str = "https://tvglznbmklajgnffgdok.supabase.co"
 key: str = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR2Z2x6bmJta2xhamduZmZnZG9rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjc3MTg1MDcsImV4cCI6MjA0MzI5NDUwN30.SM2Zmc6BdSvbTaLUl-ZSVDoQlPliMCblQTvmEIfWnOY"
-supabase: Client = create_client(url, key)    
+supabase: Client = create_client(url, key) 
 
-#consulta_fila = supabase.table('partidos').select("*").eq("id","1").execute()
-#consulta=st.write(consulta_fila)
+sql = supabase.table('porteros').select("*").execute()
+datos_consulta_tabla_partidos = sql.data
 
-consulta_tabla_partidos = supabase.table('partidos').select("*").execute()
-datos_consulta_tabla_partidos = consulta_tabla_partidos.data
-nombres_campos = datos_consulta_tabla_partidos[0].keys() # Los nombres de los campos en un diccionario
+print (datos_consulta_tabla_partidos)
 
-df_datos_consulta_partidos = pd.DataFrame(datos_consulta_tabla_partidos)
-df_datos_columnas = pd.DataFrame(nombres_campos)
 
-df = pd.DataFrame(
-    [
-       {"id": 1, "FECHA": "Juan", "HORA": "Perez", "COMPETICION": "Liga", "EQUIPO_LOCAL": "Real Madrid", "EQUIPO_VISITANTE": "Barcelona", "RESULTADO": "2-1"},
-   ]
-)
-edited_df = st.data_editor(df, num_rows="dynamic")
-#*************************************************************
-with st.form("formulario_grabacion_partidos"):
+
+
+
+st.title("PORTEROS")
+st.markdown("En esta seccion puede añadir porteros, modificar sus datos, mostrar un listado de los porteros y ver sus estadisticas")
+
+
+tab1, tab2, tab3, tab4 = st.tabs(["LISTADO", "AÑADIR", "ESTADISTICAS", "MODIFICAR"])
+
+with tab1:
+    st.header("LISTADO DE PORTEROS")
     
-    st.write("GRABACION DE PARTIDOS")
+with tab2:
+    st.header("AÑADIR UN PORTERO")
+    with st.form("formulario_grabacion_partidos"):
+        st.write("GRABACION DE PARTIDOS")
+        nombre = st.text_input('NOMBRE')
+        apellidos = st.text_input('APELLIDOS')
+        fecha_nacimiento = st.date_input('FECHA NACIMIENTO')
+        edad = st.number_input('EDAD', step=1)
+        equipo = st.selectbox('EQUIPO', ['Alevin A F7', 'Benjamin A', 'Benjamin B', 'Prebenjamin A'], index=None, placeholder='Seleccione un equipo')
+
+        boton_grabar_portero = st.form_submit_button('Grabar PORTERO')
+        if boton_grabar_portero:
+            print(fecha_nacimiento)
+            fecha_nacimiento_str = fecha_nacimiento.isoformat()
+            print(fecha_nacimiento_str)
+            supabase.table('porteros').insert({"nombre": nombre, "apellidos": apellidos, "fecha_nac": fecha_nacimiento_str, "edad": edad, "equipo": equipo,}).execute()
+
+with tab3:
+    st.header("ESTADISTICAS")
+    st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
+with tab4:
+
+    st.markdown("""
+    <style>
+    h2 {
+        color: #96D4D4;
+        font-family: Arial, sans-serif;
+    }
     
-    fecha = st.date_input('FECHA')
-    hora = st.time_input('HORA')
-    campo = st.text_input('CAMPO')
-    competicion = st.selectbox('COMPETICION', ['LIGA','Torneo','Amistoso'], index=None, placeholder='Seleccione una competicion')
+        
     
-    col1, col2 = st.columns(2)
-    with col1:
-        equipo_atco = st.selectbox('EQUIPO ATCO',("Alevin A F7", "Benjamin A", "Benjamin B", "Prebenjamin A"),index=None,placeholder="Seleccione un Equipo",
-    )
-    with col2:
-        goles_atco = st.number_input('GOLES ATLETICO', step=1)
-    
-    col3, col4 = st.columns(2)
-    with col3:
-        equipo_rival = st.text_input('EQUIPO RIVAL')
-    with col4:
-        goles_rival = st.number_input('GOLES RIVAL', step=1)
+    </style>
+    """, unsafe_allow_html=True)
 
-    col5, col6 = st.columns(2)
-    with col5:
-        portero_titular = st.selectbox('PORTERO TITULAR',("Luis Garcia", "Pedro Garcia", "Luis Fernandez", "Juan Vargas"),index=None,placeholder="Seleccione un Portero",
-    )
-    with col6:
-        min_titular = st.number_input('MINUTOS PORTERO TITULAR', step=1)
-
-    col7, col8 = st.columns(2)
-    with col7:
-        portero_suplente = st.selectbox('PORTERO SUPLENTE',("Luis Garcia", "Pedro Garcia", "Luis Fernandez", "Juan Vargas"),index=None,placeholder="Seleccione un Portero",
-    )
-    with col8:
-        min_suplente = st.number_input('MINUTOS PORTERO SUPLENTE', step=1)
+    st.header("MODIFICAR DATOS DE UN PORTERO")
+    dataframe_datos_porteros = pd.DataFrame(datos_consulta_tabla_partidos)
+    dataframe_datos_porteros['Modificar'] = ['Modificar'] * len(dataframe_datos_porteros)
+    df_editar = st.data_editor(dataframe_datos_porteros, num_rows="dynamic", key='data_editor')
 
     
-    st.form_submit_button('Grabar Partido')
-#*************************************************************
+    button_modificar_portero = st.button("Modificar Portero")
+    
+    if button_modificar_portero:
+        # Limpiar valores no finitos en la columna 'edad'
+        #df_editar['edad'] = df_editar['edad'].fillna(0).replace([float('inf'), float('-inf')], 0).astype(int)
+        
+        porteros_list = df_editar.to_dict(orient='records')
+        for portero in porteros_list:
+            
+            id_con_decimial = portero['id']
+            print(id_con_decimial)
+            id_sin_decimal = int(id_con_decimial)
+            print(id_sin_decimal)
+            edad_con_decimal = portero['edad']
+            print(edad_con_decimal)
+            edad_sin_decimal = int(edad_con_decimal)
+            print(edad_sin_decimal)
+            portero['id'] = id_sin_decimal
+            portero['edad'] = edad_sin_decimal
 
-
-
-
-
-#*************************************************************
-# This is outside the form
-st.write(fecha,hora,competicion,equipo_atco,goles_atco,equipo_rival,goles_rival)
-
-#*************************************************************
-
-
-st.write(df_datos_consulta_partidos, df_datos_columnas,df)
- 
+        print(porteros_list)
+        
+        
+        # Borrar todos los registros de la tabla 'porteros'
+        #supabase.table('porteros').delete().neq('id', 0).execute()
+        
+        # Insertar los nuevos datos en la tabla 'porteros'
+        #supabase.table('porteros').insert(porteros_list).execute()
+        supabase.table('porteros').upsert(porteros_list).execute()
+        
+        st.write("Datos modificados correctamente")
